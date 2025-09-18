4ab51083248a4ba6ae9d422c9d0f859323110090
pranav v <vinupoojavinukumar@gmail.com@email.com>
2025-09-18 22:50:30 +0530

    Add SQL scripts for Users, Employees, Agents, and Complaints tables

---
+++ classwork/login.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users (
+    user_id INT,
+    username VARCHAR(100) ,
+    email VARCHAR(150),
+    password_ VARCHAR(255),
+);
---
+++ homework/homw.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Users (
+    user_id INT,
+    username VARCHAR(100) ,
+    email VARCHAR(150),
+    password_ VARCHAR(255)
+);
+CREATE TABLE employes (
+    employe_id INT,
+    employe_name VARCHAR(100) ,
+    email VARCHAR(150),
+    password_ VARCHAR(255)
+);
+CREATE TABLE Agent (
+    agent_id INT,
+    agent_name VARCHAR(100) ,
+    email VARCHAR(150),
+    password_ VARCHAR(255)
+);
+
+CREATE TABLE Complaints (
+    complaint_id INt,
+    complaint_ VARCHAR(255)
+);
