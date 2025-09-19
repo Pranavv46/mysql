4e0b8c548a79736c7dddf1a18132099c34518754
pranav v <vinupoojavinukumar@gmail.com@email.com>
2025-09-19 10:42:15 +0530

    Add initial SQL script for students table creation and data insertion

---
+++ classwork/day2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (
+    student_id INT,
+    student_name VARCHAR(100),
+    student_age INT,
+    department VARCHAR(100),
+    grade_ VARCHAR(50)
+);
+
+Insert into students (student_id, student_name, student_age, department, grade_) values
+(1, 'Alice', 20, 'Computer Science', 'A'),
+(2, 'Bob', 22, 'Mathematics', 'B'),
+(3, 'Charlie', 21, 'Physics', 'A'),
+(4, 'David', 23, 'Chemistry', 'C'),
+(5, 'Eve', 20, 'Biology', 'B');
