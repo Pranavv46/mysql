ba6ffeb3fdf83fc295b0246dd43d0f38f1cb0492
pranav v <vinupoojavinukumar@gmail.com@email.com>
2025-09-23 11:10:49 +0530

    update classwork/day5.sql

--- classwork/day5.sql
+++ classwork/day5.sql
@@ -7,9 +7,9 @@ Create table mobiles (
 );
 
 Insert into mobiles (id, brand, model, price, stock) values
-(1, 'Samsung', 'Galaxy M14', 12000, 30)
-(2, 'Redmi', 'Note 12', 15000, 25)
-(3, 'Realme', 'Narzo 50', 13000, 20)
+(1, 'Samsung', 'Galaxy M14', 12000, 30),
+(2, 'Redmi', 'Note 12', 15000, 25),
+(3, 'Realme', 'Narzo 50', 13000, 20),
 (4, 'Samsung', 'Galaxy A23', 18000, 10
 );
 
@@ -17,7 +17,7 @@ select * from mobiles
 where price > 13000 or stock < 15;
 
 update mobiles
-set stock = stock + 5
+set stock = stock + 5,
 price = price -500
 where model = 'Narzo 50';
 
@@ -25,7 +25,7 @@ delete from mobiles
 where id = 2;
 
 select MIN(price) as Lowest_Price,
-       MAX(price) as Highest_Price,
+       MAX(price) as Highest_Price
        from mobiles;
 
 select SUM(stock) as Total_Stock
